0c46fcbfcbf48d1aa3369237cd25d84a217e0914
Phuc Le <lexuanphuc.mit@gmail.com>
2022-02-15 16:30:12 +0700

    fix config

--- src/config/index.js
+++ src/config/index.js
@@ -6,4 +6,4 @@ export const BASE_PROVIDER_URL = isProduction
 	? "https://kovan.infura.io/v3/0ce6e20f5ef04fe182c23c081d988254"
 	: "http://localhost:7545";
 
-export const NETWORK_ID = isProduction ? 42 : 57777;
+export const NETWORK_ID = isProduction ? 42 : 5777;
