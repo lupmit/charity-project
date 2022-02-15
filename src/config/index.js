a7fab5b3443f94a79113c20d600b0c703477e059
Phuc Le <lexuanphuc.mit@gmail.com>
2022-02-15 15:09:24 +0700

    deploy smartcontract

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,9 @@
-export const CHARITY_CONTRACT_ADDRESS =
-	"0x4101A67C9D47B8e86E658F403ABc5D94dF373AF5";
-
-const isDEV = false;
+const isProduction = true;
 
 export const BASE_SERVER_URL = "/api";
 
-export const BASE_PROVIDER_URL = isDEV
-	? "http://localhost:7545"
-	: "https://kovan.infura.io/v3/0ce6e20f5ef04fe182c23c081d988254";
+export const BASE_PROVIDER_URL = isProduction
+	? "https://kovan.infura.io/v3/0ce6e20f5ef04fe182c23c081d988254"
+	: "http://localhost:7545";
 
-export const NETWORK_ID = isDEV ? 5777 : 42;
+export const NETWORK_ID = isProduction ? 42 : 57777;
